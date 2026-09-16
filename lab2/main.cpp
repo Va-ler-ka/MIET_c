619ec3f1ffeaac31e31ae99daa4cc03811b47552
Va_ler_k_a <valeron.fedor@gmail.com>
2026-09-16 20:34:51 +0300

    Restructure: move lab1 into subdirectory, add lab2

---
+++ lab2/main.cpp
@@ -0,0 +1,7 @@
+#include "util.h"
+
+int main()
+{
+    HelloWorld();
+    return 0;
+}
---
+++ lab2/util/util.cpp
@@ -0,0 +1,7 @@
+#include "util.h"
+#include <iostream>
+
+void HelloWorld()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
